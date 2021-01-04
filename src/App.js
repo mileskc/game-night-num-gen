eb8385d22b20badcd2f159fc6390f173e1810d7a
Katelyn <katelyncmiles@gmail.com>
2021-01-03 20:57:53 -0500

    display generated random number with no styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [startingNumber, setStartingNumber] = useState('');
+  const [endingNumber, setEndingNumber] = useState('');
+  const [randomNumber, setRandomNumber] = useState('');
+
+  const clearInput = (event) => {
+    return (event.target.value = '');
+  };
+
+  const handleStartingChange = (event) => {
+    setStartingNumber(parseInt(event.target.value));
+  };
+
+  const handleEndingChange = (event) => {
+    setEndingNumber(parseInt(event.target.value));
+  };
+
+  const generateNumber = (event) => {
+    event.preventDefault();
+    const min = startingNumber;
+    const max = endingNumber + 1;
+    setRandomNumber(Math.floor(Math.random() * (max - min) + min));
+  };
+
+  console.log(randomNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <p>NUMBER GENERATOR</p>
+      <form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          From{' '}
+          <input
+            onChange={handleStartingChange}
+            onClick={clearInput}
+            value={startingNumber}
+            placeholder='enter number'
+          />{' '}
+          to{' '}
+          <input
+            onChange={handleEndingChange}
+            onClick={clearInput}
+            value={endingNumber}
+            placeholder='enter number'
+          />{' '}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button onClick={generateNumber} type='submit'>
+          Generate!
+        </button>
+      </form>
+
+      {randomNumber && <p>{randomNumber}</p>}
+    </>
   );
-}
+};
 
 export default App;
